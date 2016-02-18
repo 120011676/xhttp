7168be60c7644178b9d30770d36b4d1cd0d65c05
Say.li <120011676@qq.com>
2016-02-18 10:08:58 +0800

    更新build.gradle

---
+++ src/test/java/org/say/xhttp/B.java
@@ -0,0 +1,14 @@
+package org.say.xhttp;
+
+/**
+ * Created by say on 1/28/16.
+ */
+public class B {
+    public static void main(String[] args) {
+        String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
+        String html = XHttp.url("https://www.baidu.com/s")
+                .userAgent(userAgent)
+                .data("wd", "ss&sc").get().dataToString();
+        System.out.println(html);
+    }
+}
